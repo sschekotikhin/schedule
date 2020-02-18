7849dddad03afec97d367edbdc78ae436a3f6375
Marochkin Maksim <marmax199@gmail.com>
2020-02-18 18:42:15 +0300

    Добавлены кнопки навигации

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,7 @@
 import 'package:schedule/src/ui/divisions_list.dart';
 import 'package:schedule/src/ui/appbar.dart';
 import 'package:schedule/src/ui/drawer.dart';
+import 'package:schedule/src/ui/schedule_bottom_navbar.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
@@ -12,6 +13,7 @@ class App extends StatelessWidget {
       home: Scaffold(
         appBar: ScheduleAppBar(),
         drawer: ScheduleDrawer(),
+        bottomNavigationBar: ScheduleBottomNavBar(0),
         body: DivisionsList(),
       ),
     );
